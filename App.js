366e453dff22e2a11bc7c463c51b5296003cac8c
Joshva02 <109680659+Joshva02@users.noreply.github.com>
2023-04-25 13:17:13 -0400

    Initial commit
    Generated by create-expo-app 1.3.2.

--- App.js
+++ App.js
@@ -1,14 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-// import { registerRootComponent } from 'expo';
-
-import Wam from './src/Wam/index';
-
-// registerRootComponent(PaperExample);
 
 export default function App() {
   return (
-      <Wam/>
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
