011076a5dea240402dc6c03246710a37675dda0a
Joshva02 <109680659+Joshva02@users.noreply.github.com>
2023-05-15 13:47:26 -0400

    Updated App for new component logic

--- App.tsx
+++ App.tsx
@@ -1,30 +1,36 @@
-import React, { useState } from 'react';
-import { Appearance, KeyboardAvoidingView, Platform, useColorScheme } from 'react-native';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import PasswordInput from './src/components/Inputs/PasswordInput';
-import { BaseInput } from './src/components/Inputs/BaseInput';
-import ClearInputIcon, { ClearInput } from './src/icons/ClearInput';
-import { Search } from './src/icons/Search';
+import React, { useState } from "react";
+import { KeyboardAvoidingView, Platform } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import PasswordInput from "./src/components/Inputs/PasswordInput";
+import { BaseInput } from "./src/components/Inputs/BaseInput";
+import { ClearInput } from "./src/icons/ClearInput";
+import SearchIcon, { Search } from "./src/icons/Search";
+import { Eye } from "./src/icons/Eye";
 
 const App = () => {
-  const keyboardBehavior = Platform.OS === 'ios' ? 'padding' : 'height';
-  const [password, setPassword] = useState('');
+  const keyboardBehavior = Platform.OS === "ios" ? "padding" : "height";
+
+  // @todo show the contents of each input field
+  // and ensure that they are always capitalized
+  // but do not modify the text inside the input
+  // itself.
 
   return (
     <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView style={styles.container} behavior={keyboardBehavior}>
-        <BaseInput 
-        placeholder='Kevin'
-        type='phone'
-        iconPosition='left'
-        icon={<Search 
-         size={24}
-         color={'#A0A0A0'}/>}
+      <KeyboardAvoidingView
+        style={styles.container}
+        behavior={keyboardBehavior}
+      >
+        <BaseInput
+          label="Username"
+          placeholder="Enter Username Here"
+          LeftIcon={<Eye />}
         />
-        <PasswordInput  
-        placeholder="Enter your password"
-        value={password}
-        onChangeText={setPassword}      />
+        <PasswordInput
+          placeholder="Enter password"
+          label="Password"
+          description="Must be at least 8 characters long"
+        />
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -33,9 +39,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignContent: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignContent: "center",
+    justifyContent: "center",
     padding: 20,
   },
 });
