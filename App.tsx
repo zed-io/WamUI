304209db0cf57f97d4a849c3376e2caa73735dee
Joshva02 <109680659+Joshva02@users.noreply.github.com>
2023-05-18 19:26:47 -0400

    Modified App to Test New component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 import { KeyboardAvoidingView, Platform } from "react-native";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import PasswordInput from "./src/components/Inputs/PasswordInput";
 import { BaseInput } from "./src/components/Inputs/BaseInput";
-import { ClearInput } from "./src/icons/ClearInput";
-import SearchIcon, { Search } from "./src/icons/Search";
-import { Eye } from "./src/icons/Eye";
+import { NumberBaseInput } from "./src/components/Inputs/CurrencyFormatter";
+import { useFonts } from "expo-font";
 
 const App = () => {
-  const keyboardBehavior = Platform.OS === "ios" ? "padding" : "height";
+  const [fontsLoaded] = useFonts({
+    "IBM Plex Mono": require("./assets/fonts/IBMPlexMono-Bold.ttf"),
+    "CircularStd-Book": require("./assets/fonts/CircularStd-Book.ttf"),
+  });
 
-  // @todo show the contents of each input field
-  // and ensure that they are always capitalized
-  // but do not modify the text inside the input
-  // itself.
+  const keyboardBehavior = Platform.OS === "ios" ? "padding" : "height";
 
   return (
     <SafeAreaView style={styles.container}>
@@ -21,16 +19,7 @@ const App = () => {
         style={styles.container}
         behavior={keyboardBehavior}
       >
-        <BaseInput
-          label="Username"
-          placeholder="Enter Username Here"
-          LeftIcon={<Eye />}
-        />
-        <PasswordInput
-          placeholder="Enter password"
-          label="Password"
-          description="Must be at least 8 characters long"
-        />
+        <NumberBaseInput placeholder="Money money money" currency="$" />
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
@@ -43,6 +32,7 @@ const styles = StyleSheet.create({
     alignContent: "center",
     justifyContent: "center",
     padding: 20,
+    fontFamily: "CircularStd-Book",
   },
 });
 
